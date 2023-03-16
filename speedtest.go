8d7915a7d07ab7a41ea6764b8746c43dce5ec8cf
Mukul Kumar <81907379+slimdestro@users.noreply.github.com>
2023-03-16 10:39:20 +0530

    Internet speed test

---
+++ speedtest.go
@@ -0,0 +1,67 @@
+/**
+	@ internet speedtest golang implementation
+	@ slimdestro
+*/
+package speedtest
+
+import (
+	"net/http"
+	"time"
+)
+
+// Zone represents a geographical region
+type Zone struct {
+	Name string
+	URL  string
+}
+
+// SpeedTest is used to test user internet speed in different zones with different size of payload
+type SpeedTest struct {
+	Zones []Zone
+	Size  int
+}
+
+// NewSpeedTest creates a new SpeedTest instance
+func NewSpeedTest(zones []Zone, size int) *SpeedTest {
+	return &SpeedTest{
+		Zones: zones,
+		Size:  size,
+	}
+}
+
+// Test runs the speed test for the given zone and size
+func (s *SpeedTest) Test(zone Zone) (time.Duration, error) {
+	start := time.Now()
+	resp, err := http.Get(zone.URL)
+	if err != nil {
+		return 0, err
+	}
+	defer resp.Body.Close()
+
+	// read the response body
+	buf := make([]byte, s.Size)
+	_, err = resp.Body.Read(buf)
+	if err != nil {
+		return 0, err
+	}
+
+	return time.Since(start), nil
+}
+
+// Example usage
+func main() {
+	zones := []Zone{
+		{Name: "US East", URL: "https://site1"},
+		{Name: "US West", URL: "https://site2"},
+	}
+
+	st := NewSpeedTest(zones, 1024) // 1KB
+
+	for _, zone := range st.Zones {
+		duration, err := st.Test(zone)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("%s: %s\n", zone.Name, duration)
+	}
+}
