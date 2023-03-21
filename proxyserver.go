8d56fb078a750f3a7cb6e77592d68a2012d2e41d
Mukul Kumar <81907379+slimdestro@users.noreply.github.com>
2023-03-21 11:01:11 +0530

    golangproxyserver high performance with oauth

---
+++ proxyserver.go
@@ -0,0 +1,92 @@
+/**
+	@ failSafe high performance proxy server with oAuth in Golang
+	@ dlimdestro
+*/
+package main
+
+import (
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"time"
+	"log"
+	"context"
+	"github.com/gorilla/mux"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gorilla/handlers"
+)
+
+// ProxyServer struct
+type ProxyServer struct {
+	Proxy *httputil.ReverseProxy
+	Config *ProxyConfig
+}
+
+// ProxyConfig struct
+type ProxyConfig struct {
+	Target *url.URL
+	Scheme string
+	Timeout time.Duration
+	Auth bool
+	OAuth2 bool
+	MaxConcurrentRequests int
+}
+
+// NewProxyServer creates a new proxy server
+func NewProxyServer(config *ProxyConfig) *ProxyServer {
+	proxy := httputil.NewSingleHostReverseProxy(config.Target)
+	proxy.Transport = &http.Transport{
+		MaxConnsPerHost: config.MaxConcurrentRequests,
+		TLSClientConfig: &tls.Config{
+			InsecureSkipVerify: true,
+		},
+	}
+	return &ProxyServer{
+		Proxy: proxy,
+		Config: config,
+	}
+}
+
+// ServeHTTP handles the incoming requests
+func (p *ProxyServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	ctx, cancel := context.WithTimeout(r.Context(), p.Config.Timeout)
+	defer cancel()
+	r = r.WithContext(ctx)
+	if p.Config.Auth {
+		tokenString := r.Header.Get("Authorization")
+		if tokenString == "" {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			return []byte("secret"), nil
+		})
+		if err != nil {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+		if !token.Valid {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+	}
+	if p.Config.OAuth2 {
+		tokenString := r.Header.Get("Authorization")
+		if tokenString == "" {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			return []byte("secret"), nil
+		})
+		if err != nil {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+		if !token.Valid {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+	}
+	p.Proxy.ServeHTTP(w, r)
+}
